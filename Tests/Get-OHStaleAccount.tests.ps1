061f9dc6c32d9205f714fcae70a28c039764a42f
OzThe2 <owenthe2@gmail.com>
2018-11-18 20:22:25 +0000

    Added requirement for AD module

--- Tests/Get-OHStaleAccount.tests.ps1
+++ Tests/Get-OHStaleAccount.tests.ps1
@@ -7,18 +7,41 @@ if ((Get-Module).Name -contains 'ADTools') {
 Import-Module "$PSScriptRoot\..\ADTools\ADTools.psm1"
 
 InModuleScope ADTools {
+   
+
+
     describe "ADTools" { 
-        Context "Prereqs" {
+
+    BeforeAll {
+        Mock Get-ADUser {
+            [PSCustomObject]@{
+                name = 'joe'; 
+                DistinguishedName = 'CN=someone,OU=something,DC=domain,DC=root,DC=com';
+                   SamAccountName = 'Someone';
+                    LastLogonDate = '18 October 2018 19:29:55';
+                    enabled = $true
+            }
+    }
+
+        Context "Prereqs" {       
+
             It 'Passes Test-ModuleManifest' {
                 Test-ModuleManifest -Path "$PSScriptRoot\..\ADTools\ADTools.psd1"
                 $? | Should Be $true
             }
         }
 
-        #Context "Input" {
-        #    It "Should not throw when a valid OU is entered as a parameter" {
-                   
-        #    }
-        #}
+        Context "Input" {            
+            
+        }
+            It "Should not throw when a valid OU is entered as a parameter" {
+               { Get-OHStaleAccount -ou "CN=someone,OU=something,DC=domain,DC=root,DC=com" } | Should not throw
+
+            }
+
+            Assert-MockCalled Get-ADUser -Exactly 1
+
+            
+        }
     }
 }
